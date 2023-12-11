2d34aaaa73d172a3eea07ff705c46929113a95b5
Solomon Ntia <107444454+Dev-solomon@users.noreply.github.com>
2023-12-11 17:43:41 -0500

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 def hello_world(city, state):
     print(f'Hello World from {city}, {state}!')
 
 def main():
     hello_world('NYC', 'New York')
-=======
-def hello_world(city):
-    print(f'Hello World from {city}')
 
-def main():
-    hello_world('NYC')
->>>>>>> c5657fa38d7623c78514ed86e44f2fdffbacb05a
     
 main()
